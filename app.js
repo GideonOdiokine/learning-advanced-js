528eeaf044925d7b3f966e2ebadde542e0707040
Gideon Odiokine <61180360+GideonOdiokine@users.noreply.github.com>
2022-05-26 23:32:37 +0100

    added all files

---
+++ app.js
@@ -0,0 +1,59 @@
+// function b() {
+//   console.log("Called b!");
+// }
+
+// b();
+
+// console.log(a);
+
+// var a = "Hello World!";
+
+// console.log(a);
+
+// function b() {}
+
+// function a() {
+//   b();
+// }
+
+// a();
+
+// function b() {
+//   var myVar;
+//   console.log(myVar);
+// }
+
+// function a() {
+//   var myVar = 2;
+//   console.log(myVar);
+//   b();
+// }
+// var myVar = 1;
+// console.log(myVar);
+
+// a();
+
+// function b() {
+//   console.log(myVar);
+// }
+
+// function a() {
+//   var myVar = 2;
+//   b();
+// }
+
+// var myVar = 1;
+
+// a();
+
+// types of javascript
+
+//  Undefined
+// Null
+// Boolean
+// Number
+// String
+// Symbol
+
+var a = 3 < 4;
+console.log(a);
